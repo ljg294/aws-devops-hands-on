82d9546b5fe422a595e8e1b386529a2b1de7d248
EC2 Default User <ec2-user@ip-172-31-27-34.ap-northeast-1.compute.internal>
2022-11-02 07:12:11 +0000

    add Trend Micro Agent Layer

--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -12,5 +12,16 @@ export class AppStack extends cdk.Stack {
       handler: 'index.handler',
       code: lambda.Code.fromAsset('lambda/')
     });
+    
+		// Agent Layer 추가
+    const agentArn = 'arn:aws:lambda:ap-northeast-1:800880067056:layer:CloudOne-ApplicationSecurity-nodejs14_x:1';
+    const agentLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'TrendMicroAgent', agentArn);
+    fn.addLayers(agentLayer)
+    
+    // 환경 변수 추가
+    fn.addEnvironment('AWS_LAMBDA_EXEC_WRAPPER', '/opt/trend_app_protect');
+    fn.addEnvironment('TREND_AP_HELLO_URL', 'https://agents.jp-1.application.cloudone.trendmicro.com/');
+    fn.addEnvironment('TREND_AP_KEY', 'bedb1cc6-3977-4fed-8ef4-9822d77858de');
+    fn.addEnvironment('TREND_AP_SECRET', '앞서c682b84c-78c5-4b0f-8234-658b8d7403ad');
   }
 }
